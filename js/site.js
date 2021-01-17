ad358e97a2ed38787bb27d4577b6fdbc874d800e
michelle6818 <75517393+michelle6818@users.noreply.github.com>
2021-01-17 15:36:41 -0500

    setfirstmonth

--- js/site.js
+++ js/site.js
@@ -22,6 +22,13 @@ document.getElementById("totalCost").innerHTML = `$${(loan + z).toFixed(2)}`;
 
     //  TABLE
 // Months    = the term input
+// Payment   = "monthlyPmt"(q)
+// Principal = "monthlyPmt"(q) - interest payment(below)
+// Interest  =  previous remaining balance * rate/1200  LOOP...
+// Total Int = current month's interest plus previous months  LOOP...
+// Balance   = previous remaining balance - principal pmt
+
+
 // the original "previous balance" is always 25,000 (like 1st bld in SunsetHills
 //   ..so start loop at 1) Need to include the last value, so <= the length instead of <
 // need to set "remaining balance" and "prev remaining balance" for Interest and Balance
@@ -30,7 +37,7 @@ document.getElementById("totalCost").innerHTML = `$${(loan + z).toFixed(2)}`;
 // Column 1 is months
 //   document.getElementById("months").innerHTML = term; ??? needs to loop
 // Column 2 is
-  document.getElementById("pmt").innerHTML = `$${q.toFixed(2)}`;
+//   document.getElementById("pmt").innerHTML = `$${q.toFixed(2)}`;
 // Column 3 is
 // document.getElementById("prin").innerHTML = `$${(q - (prevBal * rate/1200)).toFixed(2)};`
 // Columnn 4 is
@@ -40,6 +47,16 @@ document.getElementById("totalCost").innerHTML = `$${(loan + z).toFixed(2)}`;
 // Column 6 is
 // document.getElementById("balance").innerHTML = `$${(prevBal - pmtPrinTotal).toFixed(2)};
 // start remaining/previous balance as the full loan
+
+// break into two functions -  one for the first month, then a loop
+document.getElementById("months").innerHTML = `1`;
+document.getElementById("pmt").innerHTML = `$${q.toFixed(2)}`;
+document.getElementById("prin").innerHTML = `$${(q - z).toFixed(2)}`;
+document.getElementById("int").innerHTML = `$${z.toFixed(2)}`;
+document.getElementById("totInt").innerHTML = `$${z.toFixed(2)}`; 
+document.getElementById("balance").innerHTML = `$${(loan - (q-z)).toFixed(2)}`;
+
+
 let prevBal = loan;
 let pmtPrin = [];
 let pmtInt = [];
@@ -51,17 +68,6 @@ let balance = [];
 //   let pmtPrin = monthlyPmt - 
 // }
    
-
-
-// Payment   = "monthlyPmt"(q)
-// Principal = "monthlyPmt"(q) - interest payment(below)
-// Interest  =  previous remaining balance * rate/1200  LOOP...
-// Total Int = current month's interest plus previous months  LOOP...
-// Balance   = previous remaining balance - principal pmt
-
-
-
-
 
 
 }
